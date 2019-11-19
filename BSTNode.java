2965e25ca51f4f25c0e985f7cb3607a56a3e85e0
Caroline-Cui <42626213+Caroline-Cui@users.noreply.github.com>
2019-11-19 07:40:53 -0800

    Add files via upload

---
+++ BSTNode.java
@@ -0,0 +1,13 @@
+//BST node stored the data which is word appear in english file
+//left: a left pointer points to the left node
+//right: a right pointer points to the right node
+public class BSTNode
+{
+    String data;
+    BSTNode left;
+    BSTNode right;
+    BSTNode(String item)
+    {
+        data = item;
+    }
+}
