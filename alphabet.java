8c565c13d7f7576b5f5d0c3b7d1df56a90f91dca
Caroline-Cui <42626213+Caroline-Cui@users.noreply.github.com>
2019-11-19 00:40:25 -0800

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,96 @@
+public class alphabet
+{
+    // This file, I create a switch statement to represent 27 pointers in the trie node
+    int value;
+    public alphabet(char ch)
+    {
+        switch (ch)
+        {
+            case 'a':
+                value = 0;
+                break;
+            case 'b':
+                value = 1;
+                break;
+            case 'c':
+                value = 2;
+                break;
+            case 'd':
+                value = 3;
+                break;
+            case 'e':
+                value = 4;
+                break;
+            case 'f':
+                value = 5;
+                break;
+            case 'g':
+                value = 6;
+                break;
+            case 'h':
+                value = 7;
+                break;
+            case 'i':
+                value = 8;
+                break;
+            case 'j':
+                value = 9;
+                break;
+            case 'k':
+                value = 10;
+                break;
+            case 'l':
+                value = 11;
+                break;
+            case 'm':
+                value = 12;
+                break;
+            case 'n':
+                value = 13;
+                break;
+            case 'o':
+                value = 14;
+                break;
+            case 'p':
+                value = 15;
+                break;
+            case 'q':
+                value = 16;
+                break;
+            case 'r':
+                value = 17;
+                break;
+            case 's':
+                value = 18;
+                break;
+            case 't':
+                value = 19;
+                break;
+            case 'u':
+                value = 20;
+                break;
+            case 'v':
+                value = 21;
+                break;
+            case 'w':
+                value = 22;
+                break;
+            case 'x':
+                value = 23;
+                break;
+            case 'y':
+                value = 24;
+                break;
+            case 'z':
+                value = 25;
+                break;
+            case '\'':
+                value = 26;
+                break;
+            default:
+                value = -1;
+                break;
+        }
+    }
+}
+
